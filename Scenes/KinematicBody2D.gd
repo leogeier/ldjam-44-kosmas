f34f2bc926974e78f4833bfb6b8a5a836c1cb1af
leogeier <leonard.geier@hotmail.com>
2019-04-27 17:36:09 +0200

    Add player character

---
+++ Scenes/KinematicBody2D.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var speed = 250
+var direction = Vector2()
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	direction = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		direction.x = 1
+	if Input.is_action_pressed('ui_left'):
+		direction.x = -1
+	if Input.is_action_pressed('ui_down'):
+		direction.y = 1
+	if Input.is_action_pressed('ui_up'):
+		direction.y = -1
+	move_and_slide(direction * speed)
