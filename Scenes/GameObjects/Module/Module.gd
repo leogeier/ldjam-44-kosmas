6f35e5380384456419462164a0a085b96c0c53c5
leogeier <leonard.geier@hotmail.com>
2019-04-27 22:00:10 +0200

    Add reachedEnd boolean

--- Scenes/GameObjects/Module/Module.gd
+++ Scenes/GameObjects/Module/Module.gd
@@ -5,16 +5,20 @@ var moduleType
 var direction = Vector2()
 var speed = 250
 var endPos = Vector2()
+var reachedEnd = true
 
 func throwFromTo(newStartPos: Vector2, newEndPos: Vector2):
 	position = newStartPos
 	endPos = newEndPos
+	reachedEnd = false
 
 func _physics_process(delta):
-	direction = (endPos - position).normalized()
-	var velocity = direction * speed
-	var distanceFromEnd = (position - endPos).length()
-	if (velocity * delta).length() < distanceFromEnd:
-		move_and_slide(velocity)
-	else:
-		position = endPos
+	if not reachedEnd:
+		direction = (endPos - position).normalized()
+		var velocity = direction * speed
+		var distanceFromEnd = (position - endPos).length()
+		if (velocity * delta).length() < distanceFromEnd:
+			move_and_slide(velocity)
+		else:
+			position = endPos
+			reachedEnd = true
