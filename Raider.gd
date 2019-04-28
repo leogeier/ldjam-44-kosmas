8f0431100f60264e85e09e66adb8748139c4587a
Unknown <hecobroe@gmx.de>
2019-04-28 12:40:03 +0200

    Bad getting stuck avoidance

--- Raider.gd
+++ Raider.gd
@@ -9,7 +9,10 @@ var Path
 var Goal
 
 var line = null
-export (int) var Speed = 200
+export (int) var StandardSpeed = 200
+var Speed = StandardSpeed
+export (float) var BounceForce = 0.01
+export (int) var BounceIntensity = 10000
 
 
 # Called when the node enters the scene tree for the first time.
@@ -35,14 +38,14 @@ func _process(delta):
 		#print(i)
 		
 		TempPath = get_parent().get_simple_path(position, i.position)
-		print(TempPath)
+		#print(TempPath)
 		#print(i, " Path length: ", CalculatePathLength(TempPath))
 		PathDistance
 		var TempDistance = CalculatePathLength(TempPath)
 		if(PathDistance > TempDistance):
 			Path = TempPath
 			PathDistance = TempDistance
-			print("Shortest Path was ", PathDistance, " To object:", i)
+			#print("Shortest Path was ", PathDistance, " To object:", i)
 
 		
 	
@@ -55,7 +58,8 @@ func _process(delta):
 		#print("Path:", Path[0])
 		#var DistanceToNext = global_position.distance_to(Path[0])
 		var direction = (Path[1] - position).normalized()
-		move_and_slide(direction * Speed)
+		#AdaptSpeed()
+		move_and_slide((direction )  * Speed)			#
 		$AnimatedSprite.play("Running")
 		if line != null:
 			get_tree().get_root().remove_child(line)
@@ -67,7 +71,42 @@ func _process(delta):
 		#Idle
 	pass
 	
+func Kill():
+	hide()
+	#Drop Module
 	
+	
+func AdaptSpeed():
+	print("Overlapping Bodies: ", get_node("Area2D").get_overlapping_bodies().size())
+	if(get_node("Area2D").get_overlapping_bodies().size() > 0):
+		Speed = StandardSpeed * 3
+	else:
+		Speed = StandardSpeed
+	
+func GetNearestCollisionForce():
+	var distance = INF
+	var NearestBody = null
+	var Force = Vector2(0,0)
+	var Bodies = get_node("Area2D").get_overlapping_bodies()
+	#print("Overlapping Bodies: ",Bodies)
+	for i in Bodies:
+		#print("Distance to overlapping: ", position.distance_to(i.position))
+		#position.distance_to(i)
+		var TempVector = (position - i.position) 
+		if(TempVector.x < TempVector.y):
+			TempVector.y = 0
+		else:
+			TempVector.x = 0
+		#							Vector            *      (Distance 			/     Intensity)
+		var CollisionVector = (TempVector * (position.distance_to(i.position) / BounceIntensity))
+		Force = Force + (CollisionVector)
+			
+			
+			
+	return ((-1 * Force) )
+	
+	
+
 	
 	
 	
