f79459dadbdb24da16017f705fd5974de994a28a
leogeier <leonard.geier@hotmail.com>
2019-04-28 09:35:40 +0200

    Module queue returns module directly

--- Scenes/GameObjects/Player/Player.gd
+++ Scenes/GameObjects/Player/Player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-var Module = preload("res://Scenes/GameObjects/Module/Module.tscn")
 var ModuleTypeBase = preload("res://Resources/Scripts/Modules/ModuleTypeBase.gd")
 var ModuleTypeParticles = preload("res://Resources/Scripts/Modules/ModuleTypeParticles.gd")
 var ModuleQueue = preload("res://Resources/Scripts/ModuleQueue.gd")
@@ -16,9 +15,9 @@ var moduleQueue = ModuleQueue.new()
 func _ready():
 	add_to_group("players")
 	
-	moduleQueue.push_module(ModuleTypeParticles.new())
-	moduleQueue.push_module(ModuleTypeParticles.new())
-	moduleQueue.push_module(ModuleTypeParticles.new())
+	moduleQueue.push_module_type(ModuleTypeParticles.new())
+	moduleQueue.push_module_type(ModuleTypeParticles.new())
+	moduleQueue.push_module_type(ModuleTypeParticles.new())
 	pass # Replace with function body.
 
 func handle_input():
@@ -45,13 +44,12 @@ func attack():
 	var mouseViewportPos = get_viewport().get_mouse_position()
 	var attackDir = (mouseViewportPos - viewportPos).normalized()
 	
-	var module = Module.instance()
-	module.set_module_type(moduleQueue.pop_module())
+	var module = moduleQueue.pop_module()
 	get_owner().add_child(module)
 	module.throwFromTo(position, get_global_mouse_position())
 
 func collectModule(moduleType: ModuleTypeBase):
-	moduleQueue.push_module(moduleType)
+	moduleQueue.push_module_type(moduleType)
 
 func _physics_process(delta):
 	handle_input()
